19f6a1a51040db24f3e5d14c1967b3a674c86e41
Заборовский Денис <zrivkoren@gmail.com>
2022-06-01 10:18:23 +0800

    Добавлен скелет всех моделей

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True
+    )
+    role = models.CharField(
+        max_length=16
+    )
+    # Enum: "user" "moderator" "admin"
+    # Администратор, модератор или пользователь. По умолчанию user.
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
