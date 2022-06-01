1a8c61f0d26b3a1c9a28ebed2393f025116e75a0
Заборовский Денис <zrivkoren@gmail.com>
2022-06-01 12:58:21 +0800

    В админке теперь есть все модели в простом виде

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Review, Title, Genre, Comment, Category
 
 admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Title)
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Comment)
