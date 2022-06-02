ef5e583f8d02a44c28e92ad358ecbf82881bd92b
Заборовский Денис <zrivkoren@gmail.com>
2022-06-02 21:55:54 +0800

    внедрение Simple JWT с базовыми настройками

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'api'
 
@@ -7,4 +11,6 @@ router_v1 = DefaultRouter()
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
