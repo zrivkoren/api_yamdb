37273f983f36e24c6c4f819c87f2fb03edb917c5
Заборовский Денис <zrivkoren@gmail.com>
2022-06-03 09:11:44 +0800

    сделана начинка для сериализаторов

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from reviews.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role',
+        )
+
+
+class SignupSerializer(serializers.Serializer):
+    pass
+
+
+class TokenSerializer(serializers.Serializer):
+    pass
