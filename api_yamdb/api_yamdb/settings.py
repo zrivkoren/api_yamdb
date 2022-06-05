73a0ed3262c99c585ac8636d108d9eddcf5bbbd8
Заборовский Денис <zrivkoren@gmail.com>
2022-06-05 21:06:44 +0800

    django_filters добавил в список установленных приложений

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'rest_framework_simplejwt',
     'reviews',
     'api',
