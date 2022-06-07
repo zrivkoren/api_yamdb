d89b6a30789b5239c804558b7ee6cd072f285a40
Матвей Пешков <peshkov.matvei@yandex.ru>
2022-06-08 09:14:34 +1100

    Исправление ошибок

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -4,7 +4,7 @@ from reviews.models import Title
 
 
 class TitleFilter(filter.FilterSet):
-    name = filter.CharFilter(field_name='name', lookup_expr='contains')
+    name = filter.CharFilter(field_name='name', lookup_expr='icontains')
     category = filter.CharFilter(field_name='category__slug')
     genre = filter.CharFilter(field_name='genre__slug')
 
